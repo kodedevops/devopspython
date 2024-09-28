94020d7b452fb5a11d6fa660e30f38ec5a7388d7
santoshkumarm1 <santoshkumarm1@verifone.com>
2024-09-28 16:35:45 +0530

    context update.

--- myproject/main.py
+++ myproject/main.py
@@ -1,29 +1,37 @@
-from flask import Flask, jsonify
+from flask import Blueprint, Flask, jsonify
 
 app = Flask(__name__)
+first_bp = Blueprint('firstbp', __name__, url_prefix='/python-service/first')
+
+@first_bp.route("/")
+def hello_default():
+    return jsonify(message="Hello Wold")
 
 
-@app.route("/first/hello")
+@first_bp.route("/hello")
 def hello_world():
     return jsonify(message="Hello Wold")
 
 
-@app.route("/first/list")
+@first_bp.route("/list")
 def hello_list():
     fruits = ['apple', 'banana', 'cherry', 'orange']
     return jsonify(fruits)
 
 
-@app.route("/first/map")
+@first_bp.route("/map")
 def hello_map():
     map = {'id': 1, "username": 'jai', "email": "jai@email.com"}
     return jsonify(map)
 
 
 
-@app.route("/first/health", methods=['GET'])
+@first_bp.route("/health", methods=['GET'])
 def health_check():
     return jsonify(message="OK")
+
+# Register the blueprint in the app
+app.register_blueprint(first_bp)
 
 
 if __name__ == '__main__':
